a9af886cc5449d0ec5dbeab34a76ac0e91b2a4ee
Rajat <96769224+raj108at@users.noreply.github.com>
2023-01-13 16:30:32 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# USA-State-and-cities-database
+
+The SQL file has **29.738** cities. All cities are located in the **United States**.
+
+The SQL file contains three tables:
+
+ - USA_STATES: STATE_NAME, STATE_CODE.
+
+ - USA_CITIES: ID, CITY_NAME, STATE_CODE.
+ 
+ - USA_STATE_AND_CITIES: ID, CITY_NAME, STATE_NAME.
+
+The DATABASE has all (or almost all) cities from the **United States**.
+
+## Compatibility
+All files are compatible with MySQL. 
---
+++ usa-states.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 13, 2023 at 08:02 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `prostaff-ats`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `master_locstates`
+--
+
+CREATE TABLE `master_locstates` (
+  `state_name` varchar(22) NOT NULL,
+  `state_code` char(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `master_locstates`
+--
+
+INSERT INTO `master_locstates` (`state_name`, `state_code`) VALUES
+('Alaska', 'AK'),
+('Alabama', 'AL'),
+('Arkansas', 'AR'),
+('Arizona', 'AZ'),
+('California', 'CA'),
+('Colorado', 'CO'),
+('Connecticut', 'CT'),
+('District of Columbia', 'DC'),
+('Delaware', 'DE'),
+('Florida', 'FL'),
+('Georgia', 'GA'),
+('Hawaii', 'HI'),
+('Iowa', 'IA'),
+('Idaho', 'ID'),
+('Illinois', 'IL'),
+('Indiana', 'IN'),
+('Kansas', 'KS'),
+('Kentucky', 'KY'),
+('Louisiana', 'LA'),
+('Massachusetts', 'MA'),
+('Maryland', 'MD'),
+('Maine', 'ME'),
+('Michigan', 'MI'),
+('Minnesota', 'MN'),
+('Missouri', 'MO'),
+('Mississippi', 'MS'),
+('Montana', 'MT'),
+('North Carolina', 'NC'),
+('North Dakota', 'ND'),
+('Nebraska', 'NE'),
+('New Hampshire', 'NH'),
+('New Jersey', 'NJ'),
+('New Mexico', 'NM'),
+('Nevada', 'NV'),
+('New York', 'NY'),
+('Ohio', 'OH'),
+('Oklahoma', 'OK'),
+('Oregon', 'OR'),
+('Pennsylvania', 'PA'),
+('Rhode Island', 'RI'),
+('South Carolina', 'SC'),
+('South Dakota', 'SD'),
+('Tennessee', 'TN'),
+('Texas', 'TX'),
+('Utah', 'UT'),
+('Virginia', 'VA'),
+('Vermont', 'VT'),
+('Washington', 'WA'),
+('Wisconsin', 'WI'),
+('West Virginia', 'WV'),
+('Wyoming', 'WY');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `master_locstates`
+--
+ALTER TABLE `master_locstates`
+  ADD PRIMARY KEY (`state_code`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
